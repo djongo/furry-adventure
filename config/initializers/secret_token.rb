58157336710e741d0a5f6eb9ce96dc14cf42e91b
Djongo <jonasholstein@gmail.com>
2013-05-20 17:30:51 +0200

    Initial commit of PUMA remake

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PumaApp::Application.config.secret_token = 'a0b61d809e561aa7daa4b0054ed7e980dc140bb9e7af3d0ced223039ac0e15dcd39651c633b6037b12d470696872a2e1223cc08f59c35662226ad18f3da456ac'
